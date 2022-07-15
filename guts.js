bc8ec274b0be209fe3ac8a84066878ba1251307d
Meka Seba <mekasebameka@gmail.com>
2022-07-14 19:04:28 -0700

    final product

--- guts.js
+++ guts.js
@@ -8,12 +8,13 @@ let optionOne = document.getElementById("option-one")
 let optionTwo = document.getElementById("option-two")
 
 password.addEventListener("click", function() {
+    optionOne.textContent = ""
+    optionTwo.textContent = ""
     for (let i = 0; i < 15; i++) {
     const randomOne = Math.floor(Math.random() * characters.length)
     const randomTwo = Math.floor(Math.random() * characters.length)
     optionOne.textContent += characters[randomOne]
     optionTwo.textContent += characters[randomTwo]    
-    optionOne.textContent = ""
-    optionTwo.textContent = ""
+   
 }})
 
