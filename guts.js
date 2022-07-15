df0f6c07cb7fcbee84af99acfe61e76897e200b9
Meka Seba <mekasebameka@gmail.com>
2022-07-14 18:46:18 -0700

    no comment

--- guts.js
+++ guts.js
@@ -11,7 +11,9 @@ password.addEventListener("click", function() {
     for (let i = 0; i < 15; i++) {
     const randomOne = Math.floor(Math.random() * characters.length)
     const randomTwo = Math.floor(Math.random() * characters.length)
-    optionOne.textContent = `${characters[randomOne]}`
-    optionTwo.textContent = `${characters[randomTwo]}` 
+    optionOne.textContent += characters[randomOne]
+    optionTwo.textContent += characters[randomTwo]    
+    optionOne.textContent = ""
+    optionTwo.textContent = ""
 }})
 
